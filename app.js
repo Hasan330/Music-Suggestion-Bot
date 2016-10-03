a01cac376c7ebe6f912580f641a44120242aaae0
Hasan Khader <hasan.saleh.khader@gmail.com>
2016-10-03 17:14:45 +0300

    changed default reply

--- app.js
+++ app.js
@@ -299,7 +299,7 @@ function receivedMessage(event) {
         break;
 
       default:
-        sendTextMessage(senderID, messageText);
+        sendTextMessage(senderID, "Hi friend");
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
